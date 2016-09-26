30335a8af934161cfe33029d3cde1895f4f482a2
Pavel Solovenko <solovenko@aerospace-systems.ru>
2016-09-26 14:24:46 +0300

    readme update

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@
 int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
+    QTranslator translator;
+    translator.load("qt_" + QLocale::system().name(), QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+    app.setTranslator(&translator);
     int code = 0;
     {
 
